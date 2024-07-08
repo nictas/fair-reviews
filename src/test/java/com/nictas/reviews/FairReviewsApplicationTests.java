ae5302f79d8296136517cf6783fbd736826226a7
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-07-08 19:38:54 +0300

    Implement connectivity to local DB

--- src/test/java/com/nictas/reviews/FairReviewsApplicationTests.java
+++ src/test/java/com/nictas/reviews/FairReviewsApplicationTests.java
@@ -8,6 +8,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 
 import com.nictas.reviews.service.github.settings.GitHubSettings;
 import com.nictas.reviews.service.github.settings.GitHubSettingsProvider;
@@ -15,10 +19,27 @@ import com.nictas.reviews.service.scheduled.DeveloperSyncService;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
+@ContextConfiguration(initializers = {FairReviewsApplicationTests.Initializer.class})
 class FairReviewsApplicationTests {
 
     private static final String SETTINGS_FOO_URL = "https://foo.example.com";
     private static final String SETTINGS_BAR_URL = "https://bar.example.com";
+
+    public static final FairReviewsPostgreSQLContainer POSTGRESQL_CONTAINER = FairReviewsPostgreSQLContainer
+            .getInstance();
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            POSTGRESQL_CONTAINER.start();
+            TestPropertyValues
+                    .of("spring.datasource.url=" + POSTGRESQL_CONTAINER.getJdbcUrl(),
+                            "spring.datasource.username=" + POSTGRESQL_CONTAINER.getUsername(),
+                            "spring.datasource.password=" + POSTGRESQL_CONTAINER.getPassword())
+                    .applyTo(configurableApplicationContext.getEnvironment());
+        }
+
+    }
 
     @Autowired
     private GitHubSettingsProvider settingsProvider;
