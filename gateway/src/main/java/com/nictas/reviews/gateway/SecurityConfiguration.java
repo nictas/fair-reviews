9a960a71539422f3bb5cdaadbba373d075854a02
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-07-14 17:19:44 +0300

    Add gateway module

---
+++ gateway/src/main/java/com/nictas/reviews/gateway/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.nictas.reviews.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(customizer -> customizer.pathMatchers("/rest/**")
+                .authenticated()
+                .anyExchange()
+                .permitAll())
+                .oauth2Login(Customizer.withDefaults())
+                .csrf(CsrfSpec::disable)
+                .build();
+    }
+
+}
---
+++ gateway/src/test/java/com/nictas/reviews/gateway/FairReviewsGatewayApplicationTest.java
@@ -0,0 +1,13 @@
+package com.nictas.reviews.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairReviewsGatewayApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
