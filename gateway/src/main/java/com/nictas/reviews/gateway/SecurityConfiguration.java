651df21ae5552e42fdfe2a4a2282d0ab973c1181
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2024-07-20 15:52:24 +0300

    Add CORS configuration to gateway

--- gateway/src/main/java/com/nictas/reviews/gateway/SecurityConfiguration.java
+++ gateway/src/main/java/com/nictas/reviews/gateway/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.nictas.reviews.gateway;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,10 +10,17 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
+
+    @Value("${ui-url}")
+    private String uiUrl;
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
@@ -19,6 +29,20 @@ public class SecurityConfiguration {
                 .oauth2Login(Customizer.withDefaults())
                 .csrf(CsrfSpec::disable)
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(uiUrl));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
 }
